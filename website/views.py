cd05f231c04ae1e0a8ed1a15411e69065bde5cb2
Luciano Oliveira <luciano8.oliveira@gmail.com>
2024-02-15 18:33:11 +0000

    testing 3

--- website/views.py
+++ website/views.py
@@ -901,7 +901,7 @@ def calculateGameDayClassification(gameDayID):
                         (func.sum(subquery.c.GAMESFAVOR) - func.sum(subquery.c.GAMESAGAINST)).label("GAMESDIFFERENCE"),
                         (
                             ((func.sum(subquery.c.POINTS) + func.sum(subquery.c.GAMES) / 3) * 100000) +
-                            ((func.sum(subquery.c.WINS)) +
+                            (func.sum(subquery.c.WINS)) +
                             # ((func.sum(subquery.c.WINS) / func.sum(subquery.c.GAMES)) * 10000) +
                             # ((func.sum(subquery.c.GAMESFAVOR) / (func.sum(subquery.c.GAMESFAVOR) + func.sum(subquery.c.GAMESAGAINST))) * 100) +
                             (player_age / 100)
