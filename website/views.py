b9cc24eaf8b229d1352046c1642edd258d021b69
Luciano Oliveira <luciano8.oliveira@gmail.com>
2024-02-19 16:48:01 +0000

    everything working

--- website/views.py
+++ website/views.py
@@ -59,8 +59,16 @@ def managementPlayers():
 @views.route('/rankingELO', methods=['GET', 'POST'])
 @login_required
 def rankingELO():
+    # Calcular Elo completo
+    print("before calculate")
+    calculate_ELO_full()
+    print("after calculate")
+    results = db.session.execute(
+        text(f"SELECT pl_id, pl_name, ROUND(pl_rankingNow, 2) as pl_rankingNow, ROUND(pl_totalRankingOpo, 2) as pl_totalRankingOpo, pl_wins, pl_losses, pl_totalGames FROM tb_ELO_ranking  order by pl_rankingNow desc")
+    )
+    print(f"results: {results}")
     players_data = Players.query.order_by(Players.pl_name).all()
-    return render_template("rankingELO.html", user=current_user, result=players_data)
+    return render_template("rankingELO.html", user=current_user, result=results)
 
 @views.route('/create_league', methods=['GET', 'POST'])
 @login_required
@@ -2089,3 +2097,124 @@ def func_create_games_for_gameday(gameDayID):
             
         else:
             necessary_games = 0
+
+
+def calculate_ELO_full():
+    # Delete all rows from tb_ELO_ranking
+    try:
+        db.session.execute(
+            text(f"DELETE FROM tb_ELO_ranking")
+        )
+        db.session.commit()             
+    except Exception as e:
+        print("Error1:", e)
+
+    # Delete all rows from tb_ELO_ranking_hist
+    try:
+        db.session.execute(
+            text(f"DELETE FROM tb_ELO_ranking_hist")
+        )
+        db.session.commit()
+    except Exception as e:
+        print("Error1:", e)
+
+    # Write every player with 1000 points and 0 games
+    try:
+        # Execute a SELECT query to fetch the required data from tb_players
+        players_data = db.session.execute(
+            text("SELECT pl_id, pl_name, pl_2024_ELO FROM tb_players")
+        ).fetchall()
+
+        # Extract the fetched data and construct the INSERT query
+        insert_query = """
+            INSERT INTO tb_ELO_ranking (pl_id, pl_name, pl_rankingNow, pl_totalRankingOpo, pl_wins, pl_losses, pl_totalGames)
+            VALUES (:pl_id, :pl_name, :pl_2024_ELO, 0, 0, 0, 0)
+        """
+
+        # Execute the INSERT query for each row of data fetched
+        for player in players_data:
+            db.session.execute(
+                text(insert_query),
+                {
+                    "pl_id": player[0],
+                    "pl_name": player[1],
+                    "pl_2024_ELO": player[2]
+                }
+            )
+
+        # Commit the transaction
+        db.session.commit()
+    except Exception as e:
+        print("Error2:", e)
+
+    # Select every game if league as K higher than 0
+    try:
+        r1 = db.session.execute(
+            text(f"SELECT gm_id, gm_idPlayer_A1, gm_namePlayer_A1, gm_idPlayer_A2, gm_namePlayer_A2, gm_idPlayer_B1, gm_namePlayer_B1, gm_idPlayer_B2, gm_namePlayer_B2, gm_result_A, gm_result_B, gm_idLeague, gm_date, gm_timeStart FROM tb_game WHERE gm_idLeague IN ( SELECT lg_id FROM tb_league WHERE lg_eloK > 0 AND lg_startDate >= :start_date ) AND gm_idPlayer_A1 IS NOT NULL ORDER BY gm_date ASC, gm_timeStart ASC"),
+            {"start_date": datetime(2024, 1, 1)},
+        ).fetchall()
+        
+
+        for d1 in r1:
+            # Get current ranking for A1
+            A1_ID = d1[1]
+            playerInfo = db.session.execute(text(f"SELECT pl_rankingNow FROM tb_ELO_ranking WHERE pl_id=:player_id"), {'player_id': A1_ID}).fetchone()
+            A1_ranking = playerInfo[0]
+            
+            # Get current ranking for A2
+            A2_ID = d1[3]
+            playerInfo = db.session.execute(text(f"SELECT pl_rankingNow FROM tb_ELO_ranking WHERE pl_id=:player_id"), {'player_id': A2_ID}).fetchone()
+            A2_ranking = playerInfo[0]
+            
+            # Get current ranking for B1
+            B1_ID = d1[5]
+            playerInfo = db.session.execute(text(f"SELECT pl_rankingNow FROM tb_ELO_ranking WHERE pl_id=:player_id"), {'player_id': B1_ID}).fetchone()
+            B1_ranking = playerInfo[0]
+            
+            # Get current ranking for B2
+            B2_ID = d1[7]
+            playerInfo = db.session.execute(text(f"SELECT pl_rankingNow FROM tb_ELO_ranking WHERE pl_id=:player_id"), {'player_id': B2_ID}).fetchone()
+            B2_ranking = playerInfo[0]
+
+            # Calculate current ELO from teamA and teamB
+            ranking_TeamA = (A1_ranking + A2_ranking) / 2
+            ranking_TeamB = (B1_ranking + B2_ranking) / 2
+
+            ELO_idLeague = d1[11]
+            leagueInfo = db.session.execute(text(f"SELECT lg_eloK FROM tb_league WHERE lg_id=:league_id"), {'league_id': ELO_idLeague}).fetchone()
+            ELO_K = leagueInfo[0]
+
+            # Calculate new ratings
+            if d1[9] > d1[10]:
+                # Update ratings for team A
+                # Calculate rating changes for A1
+                delta_A1 = ELO_K * (1 - (1 / (1 + 10 ** ((ranking_TeamB - A1_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_wins = pl_wins + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_A1, 'player_id': A1_ID})
+                # Calculate rating changes for A2
+                delta_A2 = ELO_K * (1 - (1 / (1 + 10 ** ((ranking_TeamB - A2_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_wins = pl_wins + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_A2, 'player_id': A2_ID})
+                # Update ratings for team B
+                # Calculate rating changes for B1
+                delta_B1 = ELO_K * (0 - (1 / (1 + 10 ** ((ranking_TeamA - B1_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_losses = pl_losses + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_B1, 'player_id': B1_ID})
+                # Calculate rating changes for B2
+                delta_B2 = ELO_K * (0 - (1 / (1 + 10 ** ((ranking_TeamA - B2_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_losses = pl_losses + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_B2, 'player_id': B2_ID})
+            elif d1[10] > d1[9]:
+                # Update ratings for team A
+                # Calculate rating changes for A1
+                delta_A1 = ELO_K * (0 - (1 / (1 + 10 ** ((ranking_TeamB - A1_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_losses = pl_losses + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_A1, 'player_id': A1_ID})
+                # Calculate rating changes for A2
+                delta_A2 = ELO_K * (0 - (1 / (1 + 10 ** ((ranking_TeamB - A2_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_losses = pl_losses + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_A2, 'player_id': A2_ID})
+                # Update ratings for team B
+                # Calculate rating changes for B1
+                delta_B1 = ELO_K * (1 - (1 / (1 + 10 ** ((ranking_TeamA - B1_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_wins = pl_wins + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_B1, 'player_id': B1_ID})
+                # Calculate rating changes for B2
+                delta_B2 = ELO_K * (1 - (1 / (1 + 10 ** ((ranking_TeamA - B2_ranking) / 400))))
+                db.session.execute(text(f"UPDATE tb_ELO_ranking SET pl_rankingNow = pl_rankingNow + :delta, pl_wins = pl_wins + 1, pl_totalGames = pl_totalGames + 1 WHERE pl_id = :player_id"), {'delta': delta_B2, 'player_id': B2_ID})
+        print("Done!")
+    except Exception as e:
+        print("Error99:", e)
